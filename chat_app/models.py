6f2c6d36fd1dbddb02f441c9b4feb562adfed10a
Vinicius dos Reis <vinicius.reis@estudante.uffs.edu.br>
2025-11-24 13:44:18 -0300

    feat: create message model and validate fields

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    user_id = models.CharField(
+      max_length=10, 
+      choices=[('user_a', 'Usuário A'), ('user_b', 'Usuário B')],
+      verbose_name="ID do Usuário"
+    )
+    message = models.TextField(verbose_name="Mensagem do Usuário")
+    response = models.TextField(verbose_name="Resposta do Bot")
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return f"[{self.user_id}] - {self.message[:10]}..."
+
+    class Meta:
+      ordering = ['sent_at']
---
+++ chat_app/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Message
+    fields = ['id', 'user_id', 'message', 'response', 'sent_at']
+    read_only_fields = ['response', 'sent_at']
