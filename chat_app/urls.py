d2d0a53da547662ab7d92534384f5ac831436189
Vinicius dos Reis <vinicius.reis@estudante.uffs.edu.br>
2025-11-24 17:52:10 -0300

    feat: add chat history route

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatView
+from .views import ChatView, HistoryView
 
 urlpatterns = [
   path('chat/', ChatView.as_view(), name='msg_send'),
+  path('history/<str:user_id>/', HistoryView.as_view(), name='history_detail'),
 ]
--- chat_app/views.py
+++ chat_app/views.py
@@ -1,9 +1,10 @@
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .serializers import MessageSerializer
 from .services import get_bot_response
+from .models import Message
 
 class ChatView(APIView):
   def post(self, request):
@@ -25,3 +26,14 @@ class ChatView(APIView):
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class HistoryView(generics.ListAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+      user_id = self.kwargs.get('user_id')
+      
+      if user_id:
+        return Message.objects.filter(user_id=user_id)
+      
+      return Message.objects.none()
