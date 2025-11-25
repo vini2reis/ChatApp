0fac5bb28c0d1d335dfa17cbc3363f575d2d8239
Vinicius dos Reis <vinicius.reis@estudante.uffs.edu.br>
2025-11-24 21:00:17 -0300

    feat: create conection with backend

---
+++ Frontend/src/hooks/chatApi.js
@@ -0,0 +1,19 @@
+import { api } from '../api.js'
+
+export async function sendMessage (user_id, message) {
+  const response = await api.post('chat/', { user_id, message })
+
+  return response.data
+}
+
+export async function getHistory (user_id) {
+  try {
+    const response = await api.get(`history/${user_id}/`)
+
+    return response.data
+  } catch (error) {
+    console.error("Erro ao buscar histórico:", error)
+
+    return [] 
+  }
+}
